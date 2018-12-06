795dd06acdbf1d8b4256f04da48454163b5fe310
doyoung Hwang <dodo4513@nhnent.com>
2018-12-06 22:13:24 +0900

    고민들...

--- src/main/java/blog/api/info/controller/InfoApiController.java
+++ src/main/java/blog/api/info/controller/InfoApiController.java
@@ -4,9 +4,11 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api(tags = "Info", description = "블로그 정보")
+@RequestMapping("info")
 @RestController
 public class InfoApiController {
 
