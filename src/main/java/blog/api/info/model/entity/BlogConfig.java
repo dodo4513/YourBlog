0ddfb3c588d8df5ab0f5624f33d83089a483d8c6
doyoung Hwang <dodo4513@nhnent.com>
2018-12-23 14:33:16 +0900

    jooq 제거, blogConfig ddl 수정.

--- src/main/java/blog/api/info/model/entity/BlogConfig.java
+++ src/main/java/blog/api/info/model/entity/BlogConfig.java
@@ -2,6 +2,7 @@ package blog.api.info.model.entity;
 
 import blog.api.info.model.enums.ConfigCode;
 import blog.common.model.entity.BasicColumn;
+import blog.common.model.entity.ImmutableBasicColumn;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,7 +14,7 @@ import javax.persistence.*;
 @Getter
 @Setter
 @Entity
-public class BlogConfig extends BasicColumn {
+public class BlogConfig extends ImmutableBasicColumn {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long no;
--- src/main/resources/db/migration/V1.4__create blog config table.sql
+++ src/main/resources/db/migration/V1.4__create blog config table.sql
@@ -3,7 +3,5 @@ CREATE TABLE blog_config
   no            BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   config_code   char(50)                          NOT NULL,
   contents      JSON                              NOT NULL,
-  delete_yn     char(1) DEFAULT 'N'               NOT NULL,
-  register_ymdt TIMESTAMP                         NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  update_ymdt   TIMESTAMP                         NOT NULL DEFAULT CURRENT_TIMESTAMP
+  register_ymdt TIMESTAMP                         NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
