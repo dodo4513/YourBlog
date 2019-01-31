d7bf354328bc91c5d300ef8ea6f3febd90c7027c
doyoung Hwang <dodo45133@gmail.com>
2019-01-31 23:26:35 +0900

    카테고리 관리 개발.

--- src/main/resources/static/js/category-list.js
+++ src/main/resources/static/js/category-list.js
@@ -1,44 +1,125 @@
 $(() => {
-  blog.category = {
-    init() {
-      console.log('category');
+  const data = [
+    {
+      text: 'Root',
+      children: []
+    }
+  ];
 
-      $('#tree').treeview({data: this.getTree()});
+  const customInternalNode =
+      `<div class="tui-tree-content-wrapper tui-tree-root-btn">
+            <button type="button" class="fa fa-laptop tui-tree-toggle-btn tui-js-tree-toggle-btn">
+              <span class="tui-ico-tree"></span>
+              {{stateLabel}}
+            </button>
+            <span class="tui-tree-text tui-js-tree-text">{{text}}</span>
+          </div>
+          <ul class="tui-tree-subtree tui-js-tree-subtree">{{{children}}}</ul>`;
+
+  const customLeafNode =
+      `<div class="tui-tree-content-wrapper tui-tree-root-btn">
+            <span class="tui-tree-text tui-js-tree-text">{{text}}</span>
+          </div>`;
+
+  blog.category = {
+    tree: null,
+    newTreeIndex: 1,
+
+    init() {
+      this.addEvent();
+      this.initTree();
     },
-    getTree() {
-      return [
-        {
-          text: 'Parent 1',
-          nodes: [
-            {
-              text: 'Child 1',
-              nodes: [
-                {
-                  text: 'Grandchild 1'
-                },
-                {
-                  text: 'Grandchild 2'
-                }
-              ]
-            },
-            {
-              text: 'Child 2'
+
+    initTree() {
+      this.tree = new tui.Tree('tree', {
+        data,
+        nodeDefaultState: 'opened',
+        template: {
+          internalNode: // Change to Mustache's format
+              '<div class="tui-tree-content-wrapper" style="padding-left: {{indent}}px">'
+              + // Example for using indent value
+              '<button type="button" class="tui-tree-toggle-btn tui-js-tree-toggle-btn">'
+              +
+              '<span class="tui-ico-tree"></span>' +
+              '{{stateLabel}}' +
+              '</button>' +
+              '<span class="tui-tree-text tui-js-tree-text">' +
+              '{{text}}' +
+              '</span>' +
+              '</div>' +
+              '<ul class="tui-tree-subtree tui-js-tree-subtree">' +
+              '{{{children}}}' + // Mustache's format
+              '</ul>',
+          leafNode:
+              '<div class="tui-tree-content-wrapper" style="padding-left: {{indent}}px">'
+              + // Example for using indent value
+              '<span class="tui-tree-text {{textClass}}">' +
+              '{{text}}' +
+              '</span>' +
+              '</div>'
+        },
+        renderTemplate(tmpl, props) {
+          const id = props.id;
+          const depth = this.getDepth(id);
+          const isFirstDepth = (this.getDepth(id) === 1);
+          const isLeaf = this.isLeaf(id);
+
+          if (isFirstDepth) { // customizing node template
+            if (isLeaf) {
+              tmpl = customLeafNode;
+            } else {
+              tmpl = customInternalNode;
             }
-          ]
-        },
-        {
-          text: 'Parent 2'
-        },
-        {
-          text: 'Parent 3'
-        },
-        {
-          text: 'Parent 4'
-        },
-        {
-          text: 'Parent 5'
+          }
+
+          props.indent = 20 * depth; // customizing indent
+
+          return Mustache.render(tmpl, props);
         }
-      ];
+      });
+
+      this.tree
+        .enableFeature('Selectable')
+        .enableFeature('Draggable', {
+          isSortable: true,
+          lineBoundary: {
+            top: 10,
+            bottom: 10
+          }
+        })
+        .enableFeature('Editable', {
+          editableClassName: this.tree.classNames.textClass,
+          dataKey: 'text'
+        });
+    },
+
+    addEvent() {
+      const $body = $('body');
+      $body.on('click', '.action', $.proxy(this.onClickAction, this));
+    },
+
+    onClickAction(e) {
+      const action = $(e.target).data('action');
+      console.log(action);
+
+      switch (action) {
+        case 'addChild':
+          this.addChild();
+          break;
+        case 'removeChild':
+          this.removeChild();
+          break;
+        default:
+          // nothing..
+      }
+    },
+
+    addChild() {
+      this.tree.add({text: `신규 카테고리${this.newTreeIndex++}`}, this.tree.getSelectedNodeId());
+    },
+
+    removeChild() {
+      this.tree.remove(this.tree.getSelectedNodeId());
     }
   };
   blog.category.init();
