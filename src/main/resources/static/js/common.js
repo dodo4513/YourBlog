9b8d5f82886e3921b44e5c2f48dd9fe249b7baf6
doyoung Hwang <dodo45133@gmail.com>
2019-05-04 19:56:12 +0900

    "/"가 없어야 상대주소로 호출

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -14,7 +14,7 @@ blog.common = {
     const p = {};
 
     p.traditional = true;
-    p.url = `/${params.url}`;
+    p.url = `${params.url}`;
     p.type = typeof params.type === 'undefined' ? 'GET' : params.type;
     p.contentType = typeof params.contentType === 'undefined' ? 'application/json' : params.contentType;
     p.async = typeof params.async === 'undefined' ? true : params.type;
