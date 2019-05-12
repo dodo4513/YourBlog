95764d0a87e88ec7687f8b89bbbc9e4458cb7e41
doyoung Hwang <dodo45133@gmail.com>
2019-05-12 19:53:23 +0900

    cors 처리 추가

---
+++ src/main/java/blog/common/config/SpringConfig.java
@@ -0,0 +1,18 @@
+package blog.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author doyoung hwang on 2019-05-12
+ */
+
+@Configuration
+public class SpringConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("*").allowedOrigins("*");
+  }
+}
