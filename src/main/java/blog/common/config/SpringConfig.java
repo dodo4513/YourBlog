e7643753eabbc419c7c666ab78aa9c40de977c56
doyoung Hwang <dodo45133@gmail.com>
2019-05-12 20:14:50 +0900

    cors 세팅

--- src/main/java/blog/common/config/SpringConfig.java
+++ src/main/java/blog/common/config/SpringConfig.java
@@ -13,6 +13,6 @@ public class SpringConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("*").allowedOrigins("*");
+    registry.addMapping("*").allowedOrigins("http://localhost:8080");
   }
 }
